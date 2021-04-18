f10a9aaaa8a2e5396b9ff0d6418a8c39e22e6a1a
ladetorres <leandrodetorres23@gmail.com>
2021-04-18 15:33:22 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("blogs");
+  eleventyConfig.addPassthroughCopy("views");
   return {
+    includes: "_includes",
+    data: "_data",
   };
 };
